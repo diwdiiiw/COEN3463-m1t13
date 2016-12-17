7ce4310ad9f11716d1717cbb77a7f5b06e9f4b02
arcdiw@gmail.com <arcdiwa@gmail.com>
2016-12-17 19:42:12 +0800

    dsada

--- COEN3463-m1t13/profile3/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><c href='border.html'></b></li>" +
-"<li><a href='../index.html'>HOME</a></li>" +
-"<li><a href='../profile3/index.html'>DIWA</a></li>" +
-"<li><a href='../profile2/index.html'>SAN JUAN</a></li>" +
-"<li><b href='border.html'></b></li>" +
-"</ul>";
